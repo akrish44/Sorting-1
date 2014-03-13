aafa4e8b6e42bfe5a9a250e358e6306f038dbb0e
patelkd <karishma1126@yahoo.com>
2014-03-13 17:39:12 -0600

    finished it!

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,39 +1,30 @@
 public class SelectionSort {
-	
-	public int[] numbers = {8,3,7,8,7,1,0};
+
 
 	public static void main(String[] args) {
+		int[] numbers = {8,3,7,8,7,1,0};
+		for (int i = 0; i < numbers.length - 1; i++) {
+			int minIndex = i;
+	 		for (int j = i + 1; j < numbers.length; j++) {
+  				if (numbers[j] < numbers[minIndex]) {
+   					minIndex = j;
+   				}
+   			}
+			swap(numbers, i, minIndex);
+		}
 		
 	}
+	
+	private static void swap(int[] numbers, int i, int j) {
+		int temp = numbers[i];
+		numbers[i] = numbers[j];
+		numbers[j] = temp;
+		System.out.println(numbers[i]);
 
-	public int[] Selection(int[] numbers) {
-		int place = 0;
-		for (int j=0; j<numbers.length; j++) {
-			int placeHolder = numbers[j];
-			int minimum = numbers[j+1];
-			for (int i=placeHolder+1; i<numbers.length-1; i++) {
-				if (numbers[i]>numbers[i+1]) {
-					minimum = numbers[i+1];
-					place = i + 1;
-				} else {
-					place = i;
-				}
-			}
-			if (placeHolder>minimum) {
-			   int 	
-			}
-		}
 	}
-
-	public void	print(int[] numbers) {
-		for (int i=0; i<numbers.length; i++) {
-			System.out.println(numbers[i]);
-		}
-	}
-
 	public boolean isSorted(int[] numbers) {
 		for (int i=0; i<numbers.length-1; i++) {
-			if (!(numbers[i]<number[i+1]) {
+			if (!(numbers[i]<numbers[i+1])) {
    			   return false;
 			}
 		}
