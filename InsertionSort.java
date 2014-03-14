0bd6f60960e6104fc7032eb59d2b5c0b9714010a
patelkd <karishma1126@yahoo.com>
2014-03-14 08:50:09 -0600

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,29 +1,49 @@
-public class InsertionSort {
- 
-    public static void main(String[] args) {
-         
-        int[] numbers = {8,3,7,8,7,1,0};
-        insertionSort(numbers);
+import java.util.Arrays;
+public class Bubble {
+  public static void main(String[] args) {
+        int[] arr = {3, 1, 6, 7, 1, 2};
+
+        System.out.println(Arrays.toString(arr));
+        swap(arr); 
     }
-     
-    private static void printNumbers(int[] numbers) {
-         
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + ", ");
+
+    private static void swap(int[] arr) {
+    int temp = 0;
+    boolean done = false;
+    while (!done) {
+        // for (int j = 1; j < (arr.length); j++) {
+        //     if (arr[j - 1] > arr[j]) {
+        //         temp = arr[j - 1];
+        //         arr[j - 1] = arr[j];
+        //         arr[j] = temp;
+        //     }
+            
+           
+        // }
+        done = true;
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i]>arr[i+1]) {
+                done = false;
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+
+            }
+            print(arr);
+        }
+       
+
+        }
+    }
+
+
+
+    private static void print(int[] arr) {
+        for(int k=0; k<arr.length; k++) {
+            
+            System.out.print(arr[k]+ " ");
+        
         }
         System.out.println("\n");
-    }
- 
-    public static void insertionSort(int numbers[]) {
-        for (int j = 1; j < numbers.length; j++) {
-            int key = numbers[j];
-            int i = j-1;
-            while ( (i > -1) && ( numbers [i] > key ) ) {
-                numbers [i+1] = numbers [i];
-                i--;
-            }
-            numbers[i+1] = key;
-            printNumbers(numbers);
-        }
     }
 }
